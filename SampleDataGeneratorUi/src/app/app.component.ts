ed98fce1b04e8c875c98957c89c2077efa745d1c
WorkComputer <josh.d.kofoot@rrd.com>
2018-07-06 12:23:24 -0500

    Basic ui is setup.

--- SampleDataGeneratorUi/src/app/app.component.ts
+++ SampleDataGeneratorUi/src/app/app.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 
+interface City {
+  name: string;
+  code: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +12,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  functionTypes: string[] = [ 'bool','ssn','firstName','lastName' ];
+  results: string[];
+
+  filterBrands(event) {
+    this.results = [];
+    for(let i = 0; i < this.functionTypes.length; i++) {
+        let brand = this.functionTypes[i];
+        if(brand.toLowerCase().indexOf(event.query.toLowerCase()) == 0) {
+            this.results.push(brand);
+        }
+    }
+  }
+
+  constructor() {
+  }
 }
--- SampleDataGeneratorUi/src/app/app.module.ts
+++ SampleDataGeneratorUi/src/app/app.module.ts
@@ -1,5 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {InputTextModule} from 'primeng/inputtext';
+import {ButtonModule} from 'primeng/button';
+import {CardModule} from 'primeng/card';
+import {AccordionModule} from 'primeng/accordion';
+import {DragDropModule} from 'primeng/dragdrop';
+import {AutoCompleteModule} from 'primeng/autocomplete';
+import {TooltipModule} from 'primeng/tooltip';
+import {DropdownModule} from 'primeng/dropdown';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations'
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +17,16 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    InputTextModule,
+    ButtonModule,
+    CardModule,
+    AccordionModule,
+    DragDropModule,
+    AutoCompleteModule,
+    TooltipModule,
+    DropdownModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
